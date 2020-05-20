f85512c2a1c0292e29082a8c076d33ced8f1d2a4
Stanislav Taran <StanislavTaran@users.noreply.github.com>
2020-05-20 21:49:59 +0300

    Updates

---
+++ precache-manifest.8155684d2f00da64bd029b45d384ddc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0f1dfbdb62a77a8a517fad5dc5eea7",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "22ca2be5005856c65d41",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c7614a1f78608590043a",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.7af1591e.chunk.css"
+  },
+  {
+    "revision": "22ca2be5005856c65d41",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.ad8a809f.chunk.js"
+  },
+  {
+    "revision": "dfd9de20f4ff60fc8200b143aaf0a20e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.ad8a809f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7614a1f78608590043a",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.079892b8.chunk.js"
+  },
+  {
+    "revision": "94630fa5ad36b27d1f48",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.d86d72da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-phonebook/precache-manifest.79ac63f2cbbac966eb6f33646096ade0.js"
+  "/goit-react-hw-05-phonebook/precache-manifest.8155684d2f00da64bd029b45d384ddc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-05-phonebook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
